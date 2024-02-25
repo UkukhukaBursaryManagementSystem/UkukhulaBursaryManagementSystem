9dd34ee9633c1c6d4986444cec4f8923afb044b2
gaboneweb <bulelanigabonewe@gmail.com>
2024-02-25 15:57:53 +0200

    Change filename of js module

---
+++ src/get-data-for-application.js
@@ -0,0 +1,56 @@
+import  {fetchData} from  './fetch_data.js'
+let data;
+
+
+const selectSexElement = document.getElementById('sex');
+data = await fetchData(`http://localhost:8080/biological-sex`);
+data.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.value = option.id;
+    optionElement.textContent = option.name;
+    selectSexElement.appendChild(optionElement);
+});
+
+const selectEthnicityElement = document.getElementById('ethnicity');
+data = await fetchData(`http://localhost:8080/ethnicity`);
+data.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.value = option.id;
+    optionElement.textContent = option.name;
+    selectEthnicityElement.appendChild(optionElement);
+});
+
+
+const selectDepartmentElement = document.getElementById('department');
+data = await fetchData(`http://localhost:8080/department`);
+data.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.value = option.id;
+    optionElement.textContent = option.name;
+    selectDepartmentElement.appendChild(optionElement);
+});
+
+const selectHeadOfDepartmentElement = document.getElementById('head-of-department');
+data = await fetchData(`http://localhost:8080/hod`);
+data.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.value = option.id;
+    optionElement.textContent = option.name;
+    selectHeadOfDepartmentElement.appendChild(optionElement);
+});
+
+
+
+
+
+
+
+
+const selectUniverisytElement = document.getElementById('university');
+data = await fetchData(`http://localhost:8080/universities`);
+data.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.value = option.id;
+    optionElement.textContent = option.name;
+    selectUniverisytElement.appendChild(optionElement);
+});
