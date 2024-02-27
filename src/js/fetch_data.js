4b1c04c3d670aeb6a9e31745e6b93e69f652c497
Magalaweng Clive Mmakola <clive.mmakola@bbd.co.za>
2024-02-27 09:14:26 +0200

    Update repo

--- src/js/fetch_data.js
+++ src/js/fetch_data.js
@@ -1,19 +1,9 @@
-const account = selectAccount(msalInstance);
-
-const accessTokenRequest = {
-  scopes: ["user.read"],
-  account: account,
-};
-
-const accessToken = aquireAccessTokenSilent(msalInstance, accessTokenRequest);
-// console.log(accessToken);
-
 async function fetchData(url) {
   const result = await fetch(url, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "Authorization": `Bearer ${accessToken}`
+      // "Authorization": `Bearer ${accessTokenAquired}`
     },
   });
   return result.json();
---
+++ src/ms-identity-javascript-v2-master/app/authConfig.js
@@ -0,0 +1,43 @@
+const msalConfig = {
+    auth: {
+        clientId: "3de2be63-d983-47bf-bd2f-92e7693477fa",
+        authority: "https://login.microsoftonline.com/common",
+        navigateToLoginRequestUrl: true
+    },
+    cache: {
+        cacheLocation: "sessionStorage",
+        storeAuthStateInCookie: false,
+    },
+    system: {	
+        loggerOptions: {	
+            loggerCallback: (level, message, containsPii) => {	
+                if (containsPii) {		
+                    return;		
+                }		
+                switch (level) {		
+                    case msal.LogLevel.Error:		
+                        console.error(message);		
+                        return;		
+                    case msal.LogLevel.Info:		
+                        console.info(message);		
+                        return;		
+                    case msal.LogLevel.Verbose:		
+                        console.debug(message);		
+                        return;		
+                    case msal.LogLevel.Warning:		
+                        console.warn(message);		
+                        return;		
+                }	
+            }	
+        }	
+    }
+};
+
+const loginRequest = {
+    scopes: ["openid", "profile"]
+};
+
+const tokenRequest = {
+    scopes: ["openid", "profile"],
+    forceRefresh: false
+};
