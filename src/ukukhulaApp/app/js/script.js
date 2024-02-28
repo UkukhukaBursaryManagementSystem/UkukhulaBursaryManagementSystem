16d2c025d74bc9455c470b80600bbae9fd1dcf34
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-28 14:27:09 +0200

    styled the form

--- src/ukukhulaApp/app/js/script.js
+++ src/ukukhulaApp/app/js/script.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const hodName = document.getElementById("hodName").value;
     fetchDataByHodName(hodName);
   });
+
   fetchAllStudentApplicationData();
+  const applicationId = document.getElementById("applicationId").value;
+  editStudentApplication(applicationId);
 });
 
 async function fetchAllStudentApplicationData() {
@@ -30,8 +33,47 @@ async function fetchDataByHodName(hodName) {
   }
 }
 
-function editStudentApplication() {}
+async function editStudentApplication(applicationId) {
+  const table = document.querySelector(".hide-table");
+  const form = document.querySelector(".hidden");
+  const submitButton = document.querySelector(".submit-button");
+  form.style.display = "none";
+  table.style.display = "flex";
+  try {
+    const response = await fetch(
+      `http://localhost:8080/student-application/${applicationId}`
+    );
+    const data = await response.json();
+    console.log(data[0].genderIdentity);
+    document.getElementById("first-name").value = data[0].firstName;
+    document.getElementById("last-name").value = data[0].lastName;
+    document.getElementById("id-number").value = data[0].idnumber;
+    document.getElementById("sex").value = data[0].genderIdentity;
+    document.getElementById("ethnicity").value = data[0].ethnicity;
+    document.getElementById("phone").value = data[0].phoneNumber;
+    document.getElementById("student-email").value = data[0].email;
+    document.getElementById("student-study-course").value =
+      data[0].courseOfStudy;
+    document.getElementById("bursary-amount").value = data[0].bursaryAmount;
+    document.getElementById("university").value = data[0].universityName;
+    document.getElementById("department").value = data[0].department;
+    document.getElementById("year-of-application").value = data[0].fundingYear;
+    document.getElementById("head-of-department").value = data[0].hodName;
+    document.getElementById("Motivation").value = data[0].motivation;
 
+    submitButton.addEventListener("click", () => {
+      // const table = document.querySelector(".hide-table")
+      table.style.display = "flex";
+      form.style.display = "none";
+    });
+    table.style.display = "none";
+    form.style.display = "flex";
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+editStudentApplication("applicationId");
 function populateTable(data) {
   const tableBody = document.getElementById("table-body");
   tableBody.innerHTML = "";
@@ -40,7 +82,7 @@ function populateTable(data) {
     const row = document.createElement("tr");
     const FullName = `${student.firstName} ${student.lastName}`;
     row.innerHTML = `
-            <td>${student.applicationID}</td>                                       
+            <td id="applicationId">${student.applicationID}</td>                                       
             <td>${FullName}</td>
             <td>${student.idnumber}</td>
             <td>${student.genderIdentity}</td>
