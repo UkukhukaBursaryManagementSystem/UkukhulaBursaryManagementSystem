b0de2ce8557fdcfe8be04bc3aa33a1483505408b
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-02-29 00:19:25 +0200

    created  populateHODOptions

--- src/ukukhulaApp/app/js/script.js
+++ src/ukukhulaApp/app/js/script.js
@@ -1,10 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const fetchButton = document.getElementById("fetch-button");
-  fetchButton.addEventListener("click", function () {
-    const hodName = document.getElementById("hodName").value;
-    fetchDataByHodName(hodName);
-  });
-
+  populateHODOptions();
   fetchAllStudentApplicationData();
   const editButtons = document.querySelectorAll(".edit-button");
   editButtons.forEach(function (button) {
@@ -49,7 +44,7 @@ async function editStudentApplication(applicationId) {
   const table = document.querySelector(".hide-table");
   const form = document.querySelector(".hidden");
   const submitButton = document.querySelector(".submit-button");
-
+  console.log(applicationId);
   try {
     const response = await fetch(
       `http://localhost:8080/student-application/${applicationId}`
@@ -103,8 +98,71 @@ async function removeStudentApplication(studentID) {
   }
 }
 
+async function populateHODOptions() {
+  const hodSelect = document.getElementById("head-of-department");
+
+  try {
+    const response = await fetch("http://localhost:8080/hod");
+    const hodData = await response.json();
+    // console.log(hodData);
+    hodSelect.innerHTML = "";
+
+    hodData.forEach((hod) => {
+      const option = document.createElement("option");
+      option.value = hod.name;
+      option.textContent = hod.name;
+      hodSelect.appendChild(option);
+      // console.log(hodData);
+    });
+    console.log(hodSelect);
+
+    hodSelect.addEventListener("change", async (event) => {
+      console.log("clicked");
+      console.log(hodData);
+      const selectedHeadOfDepartment = event.target.value;
+      if (selectedHeadOfDepartment === "all") {
+        fetchAllStudentApplicationData();
+      } else {
+        console.log(selectedHeadOfDepartment);
+        fetchDataByHodName(selectedHeadOfDepartment);
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching HOD data:", error);
+  }
+}
+
+async function universityOptions() {
+  const hodSelect = document.getElementById("head-of-department");
+
+  try {
+    const response = await fetch("http://localhost:8080/hod");
+    const hodData = await response.json();
+    hodSelect.innerHTML = "";
+
+    hodData.forEach((hod) => {
+      const option = document.createElement("option");
+      option.value = hod.name;
+      option.textContent = hod.name;
+      hodSelect.appendChild(option);
+    });
+
+    hodSelect.addEventListener("change", async (event) => {
+      const selectedHeadOfDepartment = event.target.value;
+      if (selectedHeadOfDepartment === "all") {
+        fetchAllStudentApplicationData();
+      } else {
+        fetchDataByHodName(selectedHeadOfDepartment);
+      }
+    });
+  } catch (error) {
+    console.error("Error fetching HOD data:", error);
+  }
+}
+
 function populateTable(data) {
   const tableBody = document.getElementById("table-body");
+  const hodSelect = document.getElementById("head-of-department");
   tableBody.innerHTML = "";
 
   data.forEach((student) => {
