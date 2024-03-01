d7380c030e35690d31c131f44b8106fca2214513
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-03-02 00:53:51 +0200

    Uncommented code

--- src/ukukhulaApp/app/js/script.js
+++ src/ukukhulaApp/app/js/script.js
@@ -1,29 +1,25 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const fetchButton = document.getElementById("fetch-button");
-//   fetchButton.addEventListener("click", function () {
-//     const hodName = document.getElementById("hodName").value;
-//     fetchDataByHodName(hodName);
-//   });
+document.addEventListener("DOMContentLoaded", async function () {
+  let data = [];
+  data = await fetchAllStudentApplicationData();
+  populateTableForAdmin(data);
 
-//   fetchAllStudentApplicationData();
-//   const editButtons = document.getElementsByClassName(".edit-button");
-//   editButtons.forEach(function (button) {
-//     button.addEventListener("click", function () {
-//       const applicationId = button.getAttribute("data-application-id");
-//       editStudentApplication(applicationId);
-//     });
-//   });
-//  });
+  const editButtons = document.querySelector(".edit-button");
+  const studentApplicationForm = document.querySelector(".hidden");
+  const table = document.querySelector(".hide-table");
+  editButtons.addEventListener("click", function () {
+    table.style.display = "none";
+    studentApplicationForm.style.display = "flex";
+  });
+});
 
-// async function fetchAllStudentApplicationData() {
-//   try {
-//     const response = await fetch(`http://localhost:8080/student-application`);
-//     const data = await response.json();
-//     populateTable(data);
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//   }
-// }
+async function fetchAllStudentApplicationData() {
+  try {
+    const response = await fetch(`http://localhost:8080/student-application`);
+    return response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
 // async function fetchDataByHodName(hodName) {
 //   hodName = hodName.split(" ");
@@ -38,50 +34,50 @@
 //   }
 // }
 
-// async function editStudentApplication(applicationId) {
-//   const table = document.querySelector(".hide-table");
-//   const form = document.querySelector(".hidden");
-//   const submitButton = document.querySelector(".submit-button");
+async function editStudentApplication(applicationId) {
+  const table = document.querySelector(".hide-table");
+  const form = document.querySelector(".hidden");
+  const submitButton = document.querySelector(".submit-button");
 
-//   try {
-//     const response = await fetch(
-//       `http://localhost:8080/student-application/${applicationId}`
-//     );
-//     const data = await response.json();
-//     const fieldMappings = {
-//       "first-name": "firstName",
-//       "last-name": "lastName",
-//       "id-number": "idnumber",
-//       sex: "genderIdentity",
-//       ethnicity: "ethnicity",
-//       phone: "phoneNumber",
-//       "student-email": "email",
-//       "student-study-course": "courseOfStudy",
-//       "bursary-amount": "bursaryAmount",
-//       university: "universityName",
-//       department: "department",
-//       "year-of-application": "fundingYear",
-//       "head-of-department": "hodName",
-//       Motivation: "motivation",
-//     };
+  try {
+    const response = await fetch(
+      `http://localhost:8080/student/${applicationId}`
+    );
+    const data = await response.json();
+    const fieldMappings = {
+      "first-name": "firstName",
+      "last-name": "lastName",
+      "id-number": "idnumber",
+      sex: "genderIdentity",
+      ethnicity: "ethnicity",
+      phone: "phoneNumber",
+      "student-email": "email",
+      "student-study-course": "courseOfStudy",
+      "bursary-amount": "bursaryAmount",
+      university: "universityName",
+      department: "department",
+      "year-of-application": "fundingYear",
+      "head-of-department": "hodName",
+      Motivation: "motivation",
+    };
 
-//     for (const fieldId in fieldMappings) {
-//       const dataProperty = fieldMappings[fieldId];
-//       const fieldValue = data[0][dataProperty];
-//       document.getElementById(fieldId).value = fieldValue;
-//     }
+    for (const fieldId in fieldMappings) {
+      const dataProperty = fieldMappings[fieldId];
+      const fieldValue = data[0][dataProperty];
+      document.getElementById(fieldId).value = fieldValue;
+    }
 
-//     submitButton.addEventListener("click", () => {
-//       table.style.display = "flex";
-//       form.style.display = "none";
-//     });
+    submitButton.addEventListener("click", () => {
+      table.style.display = "flex";
+      form.style.display = "none";
+    });
 
-//     form.style.display = "flex";
-//     table.style.display = "none";
-//   } catch (error) {
-//     console.error("Error fetching data:", error);
-//   }
-// }
+    form.style.display = "flex";
+    table.style.display = "none";
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
 // function populateTable(data) {
 //   const tableBody = document.getElementById("table-body");
@@ -180,6 +176,8 @@ function viewStudentApplication(student) {
 
   pop.classList.toggle("active");
 
+  document.querySelector(".student-applicationID").textContent =
+    student.applicationID;
   document.querySelector(
     ".student-name"
   ).textContent = `${student.firstName} ${student.lastName}`;
@@ -200,9 +198,3 @@ function viewStudentApplication(student) {
     student.motivation;
   document.querySelector(".comment").textContent = student.reviewerComment;
 }
-
-document.addEventListener("DOMContentLoaded", async function () {
-  let data = [];
-  data = await fetchAllStudentApplicationData();
-  populateTableForAdmin(data);
-});
