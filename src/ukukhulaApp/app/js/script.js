4171abed7d1752238cdc27fc2f7cd7434275551f
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2024-03-01 23:55:38 +0200

    formated the pop content section

--- src/ukukhulaApp/app/js/script.js
+++ src/ukukhulaApp/app/js/script.js
@@ -13,7 +13,7 @@
 //       editStudentApplication(applicationId);
 //     });
 //   });
-// });
+//  });
 
 // async function fetchAllStudentApplicationData() {
 //   try {
@@ -91,7 +91,7 @@
 //     const row = document.createElement("tr");
 //     const FullName = `${student.firstName} ${student.lastName}`;
 //     row.innerHTML = `
-//             <td id="applicationId">${student.applicationID}</td>                                       
+//             <td id="applicationId">${student.applicationID}</td>
 //             <td>${FullName}</td>
 //             <td>${student.idnumber}</td>
 //             <td>${student.genderIdentity}</td>
@@ -115,14 +115,14 @@
 //     tableBody.appendChild(row);
 //   });
 // }
+
 function populateTableForAdmin(data) {
   const tableBody = document.getElementById("table-body");
   tableBody.innerHTML = "";
-
+  const popUpCard = document.querySelector(".pop-up-card");
   data.forEach((student) => {
     const row = document.createElement("tr");
-    row.setAttribute('class', 'table-row');
-
+    row.setAttribute("class", "table-row");
 
     const FullName = `${student.firstName} ${student.lastName}`;
 
@@ -130,74 +130,79 @@ function populateTableForAdmin(data) {
     applicationNameCell.textContent = FullName;
     row.appendChild(applicationNameCell);
 
-
     const universityNameCell = document.createElement("td");
     universityNameCell.textContent = student.universityName;
     universityNameCell.setAttribute("class", "hide-mobile");
     row.appendChild(universityNameCell);
-
 
     const applicationAmountCell = document.createElement("td");
     applicationAmountCell.textContent = student.bursaryAmount;
     applicationAmountCell.setAttribute("class", "hide-mobile");
     row.appendChild(applicationAmountCell);
 
-
     const statusCell = document.createElement("td");
     statusCell.textContent = student.status;
 
     statusCell.setAttribute("class", "status review");
-    if(student.status == 'approved')    statusCell.setAttribute("class", "status approved");
-    if(student.status == 'rejected')    statusCell.setAttribute("class", "status rejected");
+    if (student.status == "approved")
+      statusCell.setAttribute("class", "status approved");
+    if (student.status == "rejected")
+      statusCell.setAttribute("class", "status rejected");
     row.appendChild(statusCell);
 
     const buttonCell = document.createElement("td");
     const viewButton = document.createElement("button");
-    viewButton.setAttribute("class", 'view-app-button');
+    const cancelButton = document.querySelector(".cancel");
+    viewButton.setAttribute("class", "view-app-button");
     viewButton.setAttribute("data-applicationID", student.applicationID);
     viewButton.textContent = "View";
     buttonCell.appendChild(viewButton);
 
-    viewButton.addEventListener("click", function() {
+    viewButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      popUpCard.style.display = "block";
       viewStudentApplication(student);
+    });
+
+    cancelButton.addEventListener("click", function (event) {
+      event.preventDefault();
+      popUpCard.style.display = "none";
     });
 
     row.appendChild(buttonCell);
 
     tableBody.appendChild(row);
-
   });
 }
 
+function viewStudentApplication(student) {
+  let pop = document.querySelector(".pop-up-content");
 
-function viewStudentApplication(student){
+  pop.classList.toggle("active");
 
-    let pop = document.querySelector('.pop-up-content');
-
-    pop.classList.toggle('active');
-
-    document.querySelector('.student-name').textContent =`${student.firstName} ${student.lastName}`;
-    document.querySelector('.student-id-number').textContent = student.idnumber;
-    document.querySelector('.student-sex').textContent = student.genderIdentity;
-    document.querySelector('.student-ethnicity').textContent = student.ethnicity;
-    document.querySelector('.student-email').textContent = student.email;
-    document.querySelector('.student-phone-number').textContent = student.phoneNumber;
-    document.querySelector('.student-university').textContent = student.universityName;
-    document.querySelector('.student-department').textContent = student.department;
-    document.querySelector('.student-study').textContent = student.courseOfStudy;
-    document.querySelector('.student-amount').textContent = student.bursaryAmount;
-    document.querySelector('.hod-name').textContent = student.hodname;
-    document.querySelector('.student-motivation').textContent = student.motivation;      
-    document.querySelector('.comment').textContent = student.reviewerComment;
-
+  document.querySelector(
+    ".student-name"
+  ).textContent = `${student.firstName} ${student.lastName}`;
+  document.querySelector(".student-id-number").textContent = student.idnumber;
+  document.querySelector(".student-sex").textContent = student.genderIdentity;
+  document.querySelector(".student-ethnicity").textContent = student.ethnicity;
+  document.querySelector(".student-email").textContent = student.email;
+  document.querySelector(".student-phone-number").textContent =
+    student.phoneNumber;
+  document.querySelector(".student-university").textContent =
+    student.universityName;
+  document.querySelector(".student-department").textContent =
+    student.department;
+  document.querySelector(".student-study").textContent = student.courseOfStudy;
+  document.querySelector(".student-amount").textContent = student.bursaryAmount;
+  document.querySelector(".hod-name").textContent = student.hodname;
+  document.querySelector(".student-motivation").textContent =
+    student.motivation;
+  document.querySelector(".comment").textContent = student.reviewerComment;
 }
 
-
-document.addEventListener('DOMContentLoaded', async function(){
-let data = [];
-data = await fetchAllStudentApplicationData();
-populateTableForAdmin(data);
+document.addEventListener("DOMContentLoaded", async function () {
+  let data = [];
+  data = await fetchAllStudentApplicationData();
+  populateTableForAdmin(data);
 });
-
-
-
